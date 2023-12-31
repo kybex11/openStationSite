e057038ac5cd803247dbc6f8e458b0b63ccf7819
kybex11 <artm.22@@internet.ru>
2023-12-31 17:36:33 +0300

    Snowflake Added to * page

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import '~/components/Snowflake.vue';
+</script>
 <template>
   <div>
+    <Snowflake/>
     <NuxtPage />
   </div>
 </template>
+
 <style>
 .page-enter-active,
 .page-leave-active {
@@ -13,4 +18,23 @@
   opacity: 0;
   filter: blur(1rem);
 }
+
+.snowflake {
+	position: fixed;
+	top: 0;
+	width: 10px;
+	height: 10px;
+	background-color: #fff;
+	border-radius: 50%;
+	animation: fall linear infinite;
+	animation-timing-function: ease-in-out;
+	animation-duration: 5s;
+}
+
+@keyframes fall {
+	to {
+		transform: translateY(100vh);
+	}
+}
+
 </style>
---
+++ components/Snowflake.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const snowflakes = ref<Array<HTMLDivElement>>([]);
+
+const createSnowflake = () => {
+  const snowflake = document.createElement('div');
+  snowflake.className = 'snowflake';
+
+  const size = Math.random() * 20 + 10;
+  snowflake.style.width = `${size}px`;
+  snowflake.style.height = `${size}px`;
+  snowflake.style.opacity = `${Math.random()}`;
+
+  // Set a random horizontal position
+  snowflake.style.left = `${Math.random() * 100}vw`;
+  snowflake.style.top = `${-Math.random() * 100}vh`; // Start above the viewport
+
+  document.body.appendChild(snowflake);
+
+  snowflakes.value.push(snowflake);
+};
+
+const animateSnowflakes = () => {
+  snowflakes.value.forEach((snowflake, index) => {
+    const speed = Math.random() * 3 + 1;
+    snowflake.style.animationDuration = `${speed}s`;
+
+    snowflake.addEventListener('animationiteration', () => {
+      // Reset position and opacity for a new animation cycle
+      snowflake.style.top = `${-Math.random() * 100}vh`;
+      snowflake.style.left = `${Math.random() * 100}vw`;
+      snowflake.style.opacity = `${Math.random()}`;
+    });
+
+    snowflake.addEventListener('animationend', () => {
+      // Remove the snowflake from the DOM and array
+      snowflake.remove();
+      snowflakes.value.splice(index, 1);
+      createSnowflake();
+    });
+  });
+};
+
+onMounted(() => {
+  // Create more snowflakes for a denser effect
+  for (let i = 0; i < 50; i++) {
+    createSnowflake();
+  }
+
+  animateSnowflakes();
+});
+</script>
