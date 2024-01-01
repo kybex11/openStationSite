0511ed33fb86c43e9929a3dbc0d7eee8a0b9dc0b
kybex11 <artm.22@@internet.ru>
2024-01-01 21:26:42 +0300

    MonacoEditor Repair

--- components/MonacoEditor.vue
+++ components/MonacoEditor.vue
@@ -3,44 +3,18 @@
 </template>
 
 <script>
-export default {
-  props: {
-    language: {
-      type: String,
-      default: 'javascript',
-    },
-    code: {
-      type: String,
-      default: '',
-    },
-  },
-  data() {
-    return {
-      editorId: `editor-${Math.random().toString(36).substring(7)}`,
-      editor: null,
-    };
-  },
-  methods: {
-    async initMonaco() {
-      if (typeof window !== 'undefined') {
-        const monaco = await import('monaco-editor');
-        this.editor = monaco.editor.create(document.getElementById(this.editorId), {
-          value: this.code,
-          language: this.language,
-          theme: 'vs-dark',
-        });
-      }
-    },
-  },
-  async mounted() {
-    await this.initMonaco();
-  },
-  beforeDestroy() {
-    if (this.editor) {
-      this.editor.dispose();
-    }
-  },
-};
+import * as monaco from 'monaco-editor';
+
+const value = `console.log("Hello, Nigger World")`
+
+const editor = monaco.editor.create(document.getElementById("editorId"), {
+  value,
+  language: "typescript",
+  automaticLayout: true,
+  cursorSmoothCaretAnimation: "on",
+  smoothScrolling: true,
+  cursorBlinking: "smooth",
+});
 </script>
 
 <style scoped>
--- pages/enginepreview.vue
+++ pages/enginepreview.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
-    <MonacoEditor language="typescript" code="code"/>
-  </div>
+  <div id="editorId"></div>
 </template>
 
-<script setup lang="ts">
-const code = 'console.log("НЕГРЫ ПИДОРАСЫ")'
+<script>
+import * as monaco from 'monaco-editor';
+
+const code = `console.log("Hello, World")`;
+const editorElement = document.getElementById('editorId');
+
+if (editorElement) {
+const editor = monaco.editor.create(editorElement, {
+  value: code,
+  language: "typescript",
+  automaticLayout: true,
+  cursorSmoothCaretAnimation: "on",
+  smoothScrolling: true,
+  cursorBlinking: "smooth",
+  theme: "vs-dark",
+});
+}
+
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'indexstyle.css',
+      crossorigin: ''
+    },
+    {
+        rel: 'stylesheet',
+        href: 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css',
+        crossorigin: ''
+    }
+  ]
+});
 </script>
